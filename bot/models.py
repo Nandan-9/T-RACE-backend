a2a6053ebe89e40560b9796e1236cd4494f3e921
a.d.devnandan <a.d.devnandan@gmail.com>
2026-07-03 02:42:49 +0530

    added contect chuck

--- bot/models.py
+++ bot/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from pgvector.django import VectorField
+from django.db import models
 # Create your models here.
 
 
@@ -8,25 +9,29 @@ from django.db import models
 
 class Botlogs(models.Model):
 
-    update_id = models.CharField(max_length=100,unique=True)
-    messsage_id = models.IntegerField(blank=False, null=False)
+    update_id = models.CharField(max_length=100, unique=True)
+    message_id = models.IntegerField(blank=False, null=False)
     message_text = models.CharField(max_length=2000, null=False)
     message_date = models.DateTimeField()
-    sender_id = models.IntegerField(null=False,blank=False)
-    sender_fist_name = models.CharField(max_length=100,unique=True)
-    sender_last_name = models.CharField(max_length=100,unique=True)
-    is_sender_bot = models.BooleanField(default=False,null= False,blank=False)
-    sender_lang_code = models.CharField(max_length=20, default="en", null= False, blank= False)
+    sender_id = models.IntegerField(null=False, blank=False)
+    sender_first_name = models.CharField(max_length=100)
+    sender_last_name = models.CharField(max_length=100)
+    is_sender_bot = models.BooleanField(default=False, null=False, blank=False)
+    sender_lang_code = models.CharField(max_length=20, default="en", null=False, blank=False)
     chat_id = models.IntegerField(blank=False, null=False)
-    sender_lang_code = models.CharField(max_length=20, default="en", null= False, blank= False)
-    chat_type = models.CharField(max_length=20, default="private", null= False, blank= False)
-
+    chat_type = models.CharField(max_length=20, default="private", null=False, blank=False)
+    response_message_text = models.CharField(max_length=2000, null=True, blank=False)
 
     def __str__(self):
-        return self.message_id
+        return str(self.message_id)
 
 
-    
+
+
+class ContextChunk(models.Model):
+    content = models.TextField()
+    embedding = VectorField(dimensions=1024)      
+    created_at = models.DateTimeField(auto_now_add=True)
     
     
 
